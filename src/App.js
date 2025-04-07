dc300dddc66c85b6e8c214860ad825204efe88bc
naveenkumar-592 <naveenkumar1355@gmail.com>
2025-04-07 21:54:36 +0530

    AWS beanstalk changes commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         TEST!
+         TEST Beanstalk!
         </p>
         <a
           className="App-link"
